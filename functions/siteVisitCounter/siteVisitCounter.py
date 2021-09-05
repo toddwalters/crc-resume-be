395e26963617d67b88a9c8423d88969b80daa0b2
toddwalters <waltodders@gmail.com>
2021-09-05 14:12:09 -0400

    Lambda and Permission Updates: - remove lambda no longer needed - update DDB update lambda with error checking - update cft, remove unecessary resources and    implement least privilege access to DDB table

--- functions/siteVisitCounter/siteVisitCounter.py
+++ functions/siteVisitCounter/siteVisitCounter.py
@@ -2,6 +2,7 @@ import logging
 import json
 import os
 import boto3
+from botocore.exceptions import ClientError
 
 LOG_LEVEL = os.environ.get('LOG_LEVEL', 'DEBUG')
 log = logging.getLogger(__name__)
@@ -17,49 +18,62 @@ def lambda_handler(event, context):
 
     log.info('Making call to update visit count table')
 
-    response = update_visit_counter(table)
-
-    log.info('Table updated new counter value set to %s', response)
-
-    return response
+    try:
+        response = update_visit_counter(table)
+    except ClientError as e:
+        print(e.response['Error']['Message'])
+    else:
+        return response
 
 
 def update_visit_counter(table):
     log.info('Attempting to update visit counter table')
 
-    updatedresponse = table.update_item(
-        Key={
-            'Site': 'myCRCSite'
-        },
-        UpdateExpression="set VisitCount = if_not_exists(VisitCount, :val0)",
-        ExpressionAttributeValues={
-            ':val0': 0
-        },
-        ReturnValues='UPDATED_NEW'
-    )
-
-    updatedresponse = table.update_item(
-        Key={
-            'Site': 'myCRCSite'
-        },
-        UpdateExpression="set VisitCount = VisitCount + :val1",
-        ExpressionAttributeValues={
-            ':val1': 1
-        },
-        ReturnValues='UPDATED_NEW'
-    )
-    return updatedresponse
+    try:
+        updatedresponse = table.update_item(
+            Key={
+                'Site': 'myCRCSite'
+            },
+            UpdateExpression="set VisitCount = if_not_exists(VisitCount, :val0)",
+            ExpressionAttributeValues={
+                ':val0': 0
+            },
+            ReturnValues='UPDATED_NEW'
+        )
+        log.info('Table Counter Attribute currently set to %s', updatedresponse)
+    except ClientError as e:
+        print(e.response['Error']['Message'])
+    try:
+        updatedresponse = table.update_item(
+            Key={
+                'Site': 'myCRCSite'
+            },
+            UpdateExpression="set VisitCount = VisitCount + :val1",
+            ExpressionAttributeValues={
+                ':val1': 1
+            },
+            ReturnValues='UPDATED_NEW'
+        )
+        log.info('Table updated new counter value set to %s', updatedresponse)
+    except ClientError as e:
+        print(e.response['Error']['Message'])
+    else:
+        return updatedresponse
 
 
 def decrement_visit_counter(table):
-    updatedresponse = table.update_item(
-        Key={
-            'Site': 'myCRCSite'
-        },
-        UpdateExpression="set VisitCount = VisitCount - :val1",
-        ExpressionAttributeValues={
-            ':val1': 1
-        },
-        ReturnValues='UPDATED_NEW'
-    )
-    return updatedresponse
+    try:
+        updatedresponse = table.update_item(
+            Key={
+                'Site': 'myCRCSite'
+            },
+            UpdateExpression="set VisitCount = VisitCount - :val1",
+            ExpressionAttributeValues={
+                ':val1': 1
+            },
+            ReturnValues='UPDATED_NEW'
+        )
+    except ClientError as e:
+        print(e.response['Error']['Message'])
+    else:
+        return updatedresponse
